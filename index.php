620cc06c83eb36c003f0517166c3d814c95a792e
gezyxx <90033618+gezyxx@users.noreply.github.com>
2022-03-01 22:57:28 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WPISZ LOGIN I HASŁO</title>
+
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    
+
+
+ <form class="box" action="index.html" method="post"> 
+    <h1>ZALOGUJ SIĘ</h1>  
+<input type="email" placeholder="E-mail">
+<input type="password" placeholder="Password">
+<input type="submit" value="LOGIN">
+</body>
+</html>
---
+++ main_menu_cheetos.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>Strefa kostek </title>
+ <META HTTP-EQUIV="Refresh" CONTENT="1; URL="http://localhost/siemka/index.php">
+</head>
+<body>
+
+<br />
+<h1> Strefa kostek </h1>
+<hr />
+
+
+
+
+
+
+
+</body>
+</html>
